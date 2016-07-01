0386559274398b7f12ab06a6ac69cb4d9ebad591
Abhishek Mathur <abhishekwebcode@users.noreply.github.com>
2016-07-01 22:02:24 +0530

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,45 @@
+class session{
+    constructor() {
+        this.type="session";
+        this.sessions=0;
+        this.sinfo=[["s_info_start"]];
+        this.tempda=null;
+        return true;
+    }
+    startd() { 
+        if (this.tempda) {
+            this.stopd();
+        }
+        this.sessions+=1;
+        this.tempda="true";
+        var d = new Date;
+        var l= {};
+        l["id"]=this.sessions;
+        l["st"]=d.getTime();
+        (this.sinfo).push(l);
+    }
+    stopd() {
+        this.tempda=null;
+        var k=(this.sinfo).length;
+        var d = new Date;
+        this.sinfo[k-1]["et"]=d.getTime();
+    }
+    get() {
+        var t={};
+        t["type"]=this.type;
+        t["sessions"]=this.sessions;
+        t["s_info"]=this.sinfo;
+        return t;
+    }
+    get_diff(id) {
+        var temr = this.sinfo[id];
+        return temr["et"]-temr["st"];
+    }
+    get_diff_sessions(id1,id2) {
+        var temr1 = this.sinfo[id1];
+        temr1=temr1["et"];
+        var temr2 = this.sinfo[id2];
+        temr2=temr2["st"];
+        return temr2-temr1;
+    }
+}
